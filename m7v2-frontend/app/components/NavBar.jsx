3762bdc292e93db9d59f13bd7afa17e36a4f8121
kevin-ko <kevin.alex.ko@gmail.com>
2024-01-06 12:19:42 +0800

    some skeleton structure

---
+++ m7v2-frontend/app/components/NavBar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import Link from 'next/Link'
+
+export default function NavBar() {
+  return (
+    <nav><h1>Majestic 7 Guesthouse</h1>
+    <Link href="/">Home</Link>
+    </nav>
+    )
+}
