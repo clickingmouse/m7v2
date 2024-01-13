66298439bbb4aed35bf6c5804ef46bd492643191
kevin-ko <kevin.alex.ko@gmail.com>
2024-01-13 12:49:09 +0800

    testing vercel 4

--- m7v2-frontend/app/handy-apps/page.jsx
+++ m7v2-frontend/app/handy-apps/page.jsx
@@ -1,15 +1,13 @@
-import React from 'react'
-import AppList from './AppList'
+import React from "react";
+//import AppList from './AppList'
 
 export default function handyApps() {
   return (
     <main>
-        <nav>
-            <h2>handy apps</h2>
-            <p>coming soon</p>
-        </nav>
-        <AppList />
-
-    </main>)
-    
+      <nav>
+        <h2>handy apps</h2>
+        <p>coming soon</p>
+      </nav>
+    </main>
+  );
 }
