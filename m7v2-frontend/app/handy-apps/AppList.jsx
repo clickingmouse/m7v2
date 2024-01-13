bf86d193e708fea050eb94beeaf14d54af1b17e6
kevin-ko <kevin.alex.ko@gmail.com>
2024-01-13 12:51:40 +0800

    testing vercel 5

--- m7v2-frontend/app/handy-apps/AppList.jsx
+++ m7v2-frontend/app/handy-apps/AppList.jsx
@@ -1,31 +1,28 @@
+import React from "react";
 
-import React from 'react'
+async function getHandyApps() {
+  // const res = await fetch('http://localhost:4000/handyapps',{
+  //     next:{
+  //         revalidate:30
+  //     }
+  // })
+  //
+  return null;
+  res.json();
 
-
-async function getHandyApps(){
-    const res = await fetch('http://localhost:4000/handyapps',{
-        next:{
-            revalidate:30
-        }
-    })
-    return res.json()
-
-    //refetching 30s
+  //refetching 30s
 }
 
 export default async function AppList() {
-
-const handyApps = await getHandyApps()
-    return (
+  const handyApps = await getHandyApps();
+  return (
     <>
-    {handyApps.map( (app) => (
-        <div key = {app.id} className = "card my-5">
-            <h3>{app.title}</h3>
-            <p>{app.body.slice(0,200)}...</p>
-
+      {handyApps.map((app) => (
+        <div key={app.id} className="card my-5">
+          <h3>{app.title}</h3>
+          <p>{app.body.slice(0, 200)}...</p>
         </div>
-    ) )
-    }
+      ))}
     </>
-  )
+  );
 }
